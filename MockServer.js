8d67946761518fc26fca6cd8b1e7c55aaeb2ba4c
priyankaBitcanny <priyanka@bitcanny.com>
2018-08-24 18:27:34 +0530

    .

--- MockServer.js
+++ MockServer.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
+const str2 =
+  '<html lang="en"><head>\n' +
+  '    <meta charset="UTF-8">\n' +
+  '    <title>Title</title>\n' +
+  '</head><script type="text/javascript">\n' +
+  '  window.onload = function() {\n' +
+  '    // Deep link to your app goes here\n' +
+  '    document.getElementById("l").src = "rentlymanager://installer/";' +
+  '\n' +
+  '    setTimeout(function() {\n' +
+  '      // Link to the App Store should go here -- only fires if deep link fails' +
+  '      window.location = "https://itunes.apple.com/us/app/my.app/id123456789?ls=1&mt=8";' +
+  '    }, 500);\n' +
+  '  };\n' +
+  '</script>' +
+  '<iframe id="l" width="1" height="1" style="visibility:hidden"></iframe>' +
+  '<body>' +
+  '<br/>Rently Manager <a href="rentlymanager://installer/ce9f0396b5d6e7bdc1689591fb77381025b34932b76e98574e43f5cfa1da0c97">Installer</a>' +
+  '<br/><br/>Firebase <a' +
+  ' href="https://managerapp.page.link/installer">Installer</a>' +
+  '</body>' +
+  '</html>'
+;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Rently Manager <a' +
-  ' href="rentlymanager://installer/ca07483102d61fce7a212c108b309e98fc6bd16a574554d57c9d8ec0ccca0b4e">Installer</a>'));
+app.get('/', (req, res) => res.send(str2));
 
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
